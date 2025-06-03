b047ad3b5c2184c672b9d6872bba64f0a8db37cd
Andrew <andymuncey@users.noreply.github.com>
2025-06-03 14:42:07 +0100

    remove unused default value

--- ExampleTasks/src/uk/ac/chester/tasks/strings/Contains.java
+++ ExampleTasks/src/uk/ac/chester/tasks/strings/Contains.java
@@ -11,7 +11,7 @@ public class Contains {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String input = "";
+        String input;
         do {
             System.out.println("Type the phrase");
             input = scanner.nextLine();
