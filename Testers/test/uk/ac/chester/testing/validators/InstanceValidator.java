e9c15e3096d6c0db2ce487c9a5c035b17edad33d
Andrew <andymuncey@users.noreply.github.com>
2025-06-05 15:43:56 +0100

    improve tests

--- Testers/test/uk/ac/chester/testing/validators/InstanceValidator.java
+++ Testers/test/uk/ac/chester/testing/validators/InstanceValidator.java
@@ -1,5 +1,7 @@
 package uk.ac.chester.testing.validators;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.opentest4j.AssertionFailedError;
 import uk.ac.chester.testing.InstanceTester;
@@ -7,43 +9,65 @@ import uk.ac.chester.testing.handlers.InstanceTestEventHandlerEN;
 import uk.ac.chester.testing.testclasses.PointTestClass;
 import uk.ac.chester.testing.testclasses.TestClass;
 
+import java.lang.reflect.InvocationTargetException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InstanceValidator {
 
+    private InstanceTester<PointTestClass> tester;
 
-    //region failing tests
+    @BeforeEach
+    public void setUp() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        tester = new InstanceTester<>(PointTestClass.class,  new InstanceTestEventHandlerEN(), 5,0);
+
+    }
+
+    @AfterEach
+    public void tearDown()  {
+        tester = null;
+    }
+
+
+
     @Test
     public void exceptionThrowing(){
         InstanceTester<TestClass> tester = new InstanceTester<>(TestClass.class,  new InstanceTestEventHandlerEN());
-        tester.executeMethod(void.class,"exceptionThrower"); //should throw the exception from the method being tested
+        assertThrows(RuntimeException.class, () -> {
+            tester.executeMethod(void.class,"exceptionThrower"); //should throw the div/0 exception from the method being tested
+        });
     }
 
     @Test
     public void invalidConstruction(){
         InstanceTester<PointTestClass> tester = new InstanceTester<>(PointTestClass.class,  new InstanceTestEventHandlerEN(), "not","valid","params");
-        tester.getFieldValue(int.class, "x"); //field is valid but above construction is not
+
+        Throwable error = assertThrows(AssertionFailedError.class, () -> {
+            tester.getFieldValue(int.class, "x"); //field is valid but above construction is not
+        });
+        assertEquals("No constructed instance of the class is available to evaluate", error.getMessage());
     }
 
     @Test
     public void nonExistentMethod(){
         InstanceTester<PointTestClass> tester = new InstanceTester<>(PointTestClass.class,  new InstanceTestEventHandlerEN(), 1,2);
-        tester.executeNonReturningMethod("nonExistentMethod", 1,2,3);
+        Throwable error = assertThrows(AssertionFailedError.class, () -> {
+            tester.executeNonReturningMethod("nonExistentMethod", 1, 2, 3);
+        });
+        assertEquals("Unable to invoke method nonExistentMethod", error.getMessage());
     }
 
 
-    //region passing tests
+
     @Test
     public void nonExistentField(){
-        InstanceTester<PointTestClass> tester = new InstanceTester<>(PointTestClass.class,  new InstanceTestEventHandlerEN(), 1,2);
+       // InstanceTester<PointTestClass> tester = new InstanceTester<>(PointTestClass.class,  new InstanceTestEventHandlerEN(), 1,2);
         AssertionFailedError thrown = assertThrows(AssertionFailedError.class,
                 () -> tester.getFieldValue(int.class, "nonExistentField"),
                 "Expected failure");
         assert(thrown.getMessage().contains("Unable to retrieve value from field"));
     }
-
-
 
 
     @Test
@@ -61,17 +85,21 @@ public class InstanceValidator {
     public void property(){
         InstanceTester<PointTestClass> tester = new InstanceTester<>(PointTestClass.class,  new InstanceTestEventHandlerEN(), 0,0);
         tester.executeNonReturningMethod("setX",5);
-        Integer gotValue = tester.executeMethod(Integer.class, "getX");
-        assertEquals(Integer.valueOf(5),gotValue);
+        Integer gotValue = tester.executeMethod(int.class, "getX");
+        assertEquals(5,gotValue);
     }
 
 
     @Test
+    public void getXByName(){
+        int gotValue = tester.executeMethod(int.class, "getX");
+        assertEquals(5,gotValue);
+    }
+
+    @Test
     public void getX(){
-        InstanceTester<PointTestClass> tester = new InstanceTester<>(PointTestClass.class,  new InstanceTestEventHandlerEN(), 0,0);
-        tester.executeNonReturningMethod("setX",5);
-        Integer gotValue = tester.executeMethod(Integer.class, null);
-        assertEquals(Integer.valueOf(5),gotValue);
+        Integer gotValue = tester.executeMethodMatchingCaller(int.class);
+        assertEquals(5,gotValue);
     }
 
     @Test
