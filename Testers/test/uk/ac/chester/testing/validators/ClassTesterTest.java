b772827ff1b96a38e09e2a27c94f6820d42fea81
Andrew <andymuncey@users.noreply.github.com>
2025-06-05 17:41:20 +0100

    address warnings

--- Testers/test/uk/ac/chester/testing/validators/ClassTesterTest.java
+++ Testers/test/uk/ac/chester/testing/validators/ClassTesterTest.java
@@ -7,13 +7,21 @@ import uk.ac.chester.testing.ClassTester;
 import uk.ac.chester.testing.testclasses.TestClass;
 import uk.ac.chester.testing.handlers.ClassTestEventHandlerEN;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ClassTesterTest {
 
+
+
     @Test
     public void testPublicField() {
+
+
         Object x = new Object(){
+            @SuppressWarnings("unused")
             public String publicField;
         };
         ClassTestEventHandlerEN handler = new ClassTestEventHandlerEN();
@@ -25,7 +33,9 @@ public class ClassTesterTest {
 
     @Test
     public void testBadFieldName() {
+
         Object x = new Object(){
+            @SuppressWarnings("unused")
             private String MY_STRING;
         };
         ClassTestEventHandlerEN handler = new ClassTestEventHandlerEN();
@@ -35,13 +45,16 @@ public class ClassTesterTest {
         assertEquals("fieldNameUnconventional", TestUtilities.firstNonTestingMethodName(thrown.getStackTrace()));
     }
 
+
+    private static class SerializableClass implements Serializable {
+        @Serial
+        private static final long serialVersionUID = 1L;
+    }
+
     @Test
     public void testSerialVersionUID() {
-        Object x = new Object() {
-            private static final long serialVersionUID = 1L;
-        };
         ClassTestEventHandlerEN handler = new ClassTestEventHandlerEN();
-        ClassTester<?> t  = new ClassTester<>(x.getClass(),  handler);
+        ClassTester<SerializableClass> t  = new ClassTester<>(SerializableClass.class,  handler);
         t.checkFields();
     }
 
@@ -56,6 +69,7 @@ public class ClassTesterTest {
     @Test
     public void testMethodNameInvalid(){
         Object x = new Object(){
+            @SuppressWarnings("unused")
             private String BadMethodName(){return "hello";}
         };
         ClassTestEventHandlerEN handler = new ClassTestEventHandlerEN();
@@ -69,6 +83,7 @@ public class ClassTesterTest {
     @Test
     public void testMethodParamNameInvalid(){
         Object x = new Object(){
+            @SuppressWarnings("unused")
             private String someMethod(String BadName){return "hello";}
         };
         ClassTestEventHandlerEN handler = new ClassTestEventHandlerEN();
@@ -79,7 +94,8 @@ public class ClassTesterTest {
     }
 
 
-    private class BadConstructorParamClass {
+    private static class BadConstructorParamClass {
+        @SuppressWarnings("unused")
         private BadConstructorParamClass(String BadConstructorParamName){ }
     }
     @Test
