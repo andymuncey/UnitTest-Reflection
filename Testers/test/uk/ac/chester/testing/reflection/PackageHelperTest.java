21d7875bb07d3096e1f298bb069f6bb8bd04b376
Andrew <andymuncey@users.noreply.github.com>
2025-06-05 15:45:37 +0100

    fix test

--- Testers/test/uk/ac/chester/testing/reflection/PackageHelperTest.java
+++ Testers/test/uk/ac/chester/testing/reflection/PackageHelperTest.java
@@ -3,6 +3,7 @@ package uk.ac.chester.testing.reflection;
 
 import org.junit.jupiter.api.Test;
 import uk.ac.chester.testing.reflection.PackageHelper;
+import uk.ac.chester.testing.testclasses.TestClass;
 
 import java.util.Set;
 
@@ -12,13 +13,17 @@ public class PackageHelperTest {
 
     @Test
     public void findClasses() {
+
+        //need to declare class here as package will not be loaded otherwise
+        TestClass c = new TestClass();
+
         Set<Class<?>> classes = PackageHelper.findClasses("TestClass");
         assertFalse(classes.isEmpty());
     }
 
     @Test
     public void findClass() {
-        String packageName = "uk.ac.chester.testing";
+        String packageName = "uk.ac.chester.testing.testclasses";
         assertTrue(PackageHelper.findClass("TestClass", packageName).isPresent());
         assertFalse(PackageHelper.findClass("NonExistentClass", packageName).isPresent());
     }
