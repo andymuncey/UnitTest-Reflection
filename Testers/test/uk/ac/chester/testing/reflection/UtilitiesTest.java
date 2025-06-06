7ebd9bc57c8a4de0c5893d99a520aa06a1760aea
Andrew <andymuncey@users.noreply.github.com>
2025-06-06 14:45:44 +0100

    remove unused code

--- Testers/test/uk/ac/chester/testing/reflection/UtilitiesTest.java
+++ Testers/test/uk/ac/chester/testing/reflection/UtilitiesTest.java
@@ -1,10 +1,6 @@
 package uk.ac.chester.testing.reflection;
 
-
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -96,24 +92,4 @@ public class UtilitiesTest {
         assertEquals(Integer.class, Utilities.unBox(int.class, 4).getClass());
     }
 
-
-
-    //copied from https://www.baeldung.com/java-object-primitive-type
-
-    public static boolean isPrimitiveType(Object source) {
-        final Map<Class<?>, Class<?>> WRAPPER_TYPE_MAP;
-
-            WRAPPER_TYPE_MAP = new HashMap<Class<?>, Class<?>>(16);
-            WRAPPER_TYPE_MAP.put(Integer.class, int.class);
-            WRAPPER_TYPE_MAP.put(Byte.class, byte.class);
-            WRAPPER_TYPE_MAP.put(Character.class, char.class);
-            WRAPPER_TYPE_MAP.put(Boolean.class, boolean.class);
-            WRAPPER_TYPE_MAP.put(Double.class, double.class);
-            WRAPPER_TYPE_MAP.put(Float.class, float.class);
-            WRAPPER_TYPE_MAP.put(Long.class, long.class);
-            WRAPPER_TYPE_MAP.put(Short.class, short.class);
-            WRAPPER_TYPE_MAP.put(Void.class, void.class);
-
-        return WRAPPER_TYPE_MAP.containsKey(source.getClass());
-    }
 }
